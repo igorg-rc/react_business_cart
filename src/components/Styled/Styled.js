86a533e81eacc7ff084e272021eb5820c3d74e36
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-07 05:52:37 +0200

    changed down button

--- src/components/Styled/Styled.js
+++ src/components/Styled/Styled.js
@@ -6,9 +6,8 @@ import classes from './Styled.module.css';
 export default function Styled () {
   return (
     <div className={classes.Background}>
-      <div className="container">
-        
-        <div className={classes.HelloBox.concat(' ', "white-text")}>
+      <div className={classes.HelloBox.concat(' ', "white-text")}>
+        <div className="container">  
           <img src={mainLogo} alt="logo" className={classes.LogoImage} />
           <h1>Bonfire<span className="teal-text">Apps</span></h1>
           <h5>Flaming passion in creating the future</h5>
@@ -20,11 +19,10 @@ export default function Styled () {
             smooth={true}
             offset={-70}
             duration={500}
-          ><i className="medium material-icons teal-text">arrow_downward</i>
+          ><i className="medium material-icons orange-text text-darken-1">arrow_downward</i>
           </Link>
+          </div>
         </div>
-        
-      </div>
     </div>
   );
 }
