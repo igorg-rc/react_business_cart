6eeac6eaeb592c8730c8c7267b70033ca0274238
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-07 17:01:53 +0200

    changed icons

--- src/components/Technologies/TechnologiesData.js
+++ src/components/Technologies/TechnologiesData.js
@@ -1,26 +1,26 @@
 export const technologies = { 
   "frontend": [
-    {"title":"Javascript","imgUrl":"img/layout/icons/frontend/javascript-144.png"},
-    {"title":"React js","imgUrl":"img/layout/icons/frontend/react-144.png"},
-    {"title":"Material UI","imgUrl":"img/layout/icons/frontend/material-ui-144.png"},
-    {"title":"Bootstrap","imgUrl":"img/layout/icons/frontend/bootstrap-144.png"},
-    {"title":"jQuery","imgUrl":"img/layout/icons/frontend/jquery-144.png"},
-    {"title":"React js","imgUrl":"img/layout/icons/frontend/sass-144.png"}
+    {"title":"Javascript","imgUrl":"img/layout/icons/teal/frontend/javascript.png"},
+    {"title":"React js","imgUrl":"img/layout/icons/teal/frontend/react.png"},
+    {"title":"Material UI","imgUrl":"img/layout/icons/teal/frontend/materialui.png"},
+    {"title":"Bootstrap","imgUrl":"img/layout/icons/teal/frontend/bootstrap.png"},
+    {"title":"jQuery","imgUrl":"img/layout/icons/teal/frontend/jquery.png"},
+    {"title":"React js","imgUrl":"img/layout/icons/teal/frontend/sass.png"}
   ], 
   "backend": [
-    {"title":"Node js","imgUrl":"img/layout/icons/backend/nodejs-144.png"},
-    {"title":"MySQL","imgUrl":"img/layout/icons/backend/icons8-mysql-logo-144.png"},
-    {"title":"PostreSQL","imgUrl":"img/layout/icons/backend/postgresql-144.png"},
-    {"title":"Mongo DB","imgUrl":"img/layout/icons/backend/mongodb-144.png"},
-    {"title":"GraphQL","imgUrl":"img/layout/icons/backend/graphql-144.png"},
-    {"title":"Node js","imgUrl":"img/layout/icons/backend/nodejs-144.png"}
+    {"title":"Node js","imgUrl":"img/layout/icons/teal/backend/node-js.png"},
+    {"title":"Express js","imgUrl":"img/layout/icons/teal/backend/express_js.png"},
+    {"title":"MySQL","imgUrl":"img/layout/icons/teal/backend/mysql.png"},
+    {"title":"PostgreSQL","imgUrl":"img/layout/icons/teal/backend/postgresql.png"},
+    {"title":"Mongo DB","imgUrl":"img/layout/icons/teal/backend/mongodb.png"},
+    {"title":"GraphQL","imgUrl":"img/layout/icons/teal/backend/graphql.png"}
   ],
   "tools": [
-    {"title":"Github","imgUrl":"img/layout/icons/tools/github-144.png"},
-    {"title":"Heroku","imgUrl":"img/layout/icons/tools/heroku-144.png"},
-    {"title":"Trello","imgUrl":"img/layout/icons/tools/trello-144.png"},
-    {"title":"Webpack","imgUrl":"img/layout/icons/tools/webpack-144.png"},
-    {"title":"NPM","imgUrl":"img/layout/icons/tools/npm-144.png"},
-    {"title":"Postman","imgUrl":"img/layout/icons/tools/postman-api-144.png"}
+    {"title":"Github","imgUrl":"img/layout/icons/teal/tools/github.png"},
+    {"title":"Heroku","imgUrl":"img/layout/icons/teal/tools/heroku.png"},
+    {"title":"Trello","imgUrl":"img/layout/icons/teal/tools/trello.png"},
+    {"title":"Webpack","imgUrl":"img/layout/icons/teal/tools/webpack.png"},
+    {"title":"NPM","imgUrl":"img/layout/icons/teal/tools/npm.png"},
+    {"title":"Postman","imgUrl":"img/layout/icons/teal/tools/postman.png"}
   ]
 };
