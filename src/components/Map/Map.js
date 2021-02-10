2ef3ae5b0e922e6baa0d1dfc366de04dd0973c6a
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-10 15:08:44 +0200

    changed map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,7 +7,7 @@ export default function Map() {
         <ScrollAnimation animateIn='fadeIn'>
           <div id="map">
           <h1 className="block-title teal-text text-lighten-5">Where are we from</h1>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10678553.97279632!2d26.807337545423692!3d50.02254332946035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dc673dfa85bb03%3A0x7e675cd9074d3f4a!2sZaporizhzhia%2C%20Zaporizhia%20Oblast%2C%2069000!5e0!3m2!1sen!2sua!4v1612466645224!5m2!1sen!2sua" 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5263929.141591379!2d27.566887786042674!3d49.6947690877247!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dc60cd2a22a54d%3A0x2b82afc3e2120d51!2sMayakovs&#39;koho%20Ave%2C%2011a%2C%20Zaporizhzhia%2C%20Zaporiz&#39;ka%20oblast%2C%2069000!5e0!3m2!1sen!2sua!4v1612962350652!5m2!1sen!2sua" 
               width="600" 
               height="450" 
               frameborder="0" 
