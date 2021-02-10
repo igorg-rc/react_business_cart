314ac8206d7a6a119416cbb8b9e95fc17eb624d3
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-10 12:39:46 +0200

    changed location of pages center

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,7 +6,7 @@ export default function Map() {
       <div className="teal">
         <ScrollAnimation animateIn='fadeIn'>
           <div id="map">
-          <h2 className="block-subtitle teal-text text-lighten-5">Where are we from</h2>
+          <h1 className="block-title teal-text text-lighten-5">Where are we from</h1>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10678553.97279632!2d26.807337545423692!3d50.02254332946035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dc673dfa85bb03%3A0x7e675cd9074d3f4a!2sZaporizhzhia%2C%20Zaporizhia%20Oblast%2C%2069000!5e0!3m2!1sen!2sua!4v1612466645224!5m2!1sen!2sua" 
               width="600" 
               height="450" 
